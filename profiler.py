66544508ebf2a30e96b4d26943befafa29c37605
misakisuna705 <misakisuna705@gmail.com>
2022-10-05 00:33:45 +0800

    commit

--- profiler.py
+++ profiler.py
@@ -1,7 +1,8 @@
 # !/usr/bin/env python3
 
-import subprocess
 import csv
+import re
+import subprocess
 
 
 class Profiler(object):
@@ -45,66 +46,89 @@ class Profiler(object):
 
         # self-defined parameters
         governor = governors[2]
-        policy = policies[0]
-        frequency = frequencies[0][9]
-        core = '01'
 
-        # set policy parameters
-        subprocess.run(["adb", "shell", "echo 0 > /sys/devices/system/cpu/cpufreq/" + policy + "/scaling_min_freq"])
-        subprocess.run(["adb", "shell", "echo 99999999 > /sys/devices/system/cpu/cpufreq/" + policy + "/scaling_max_freq"])
-        subprocess.run(["adb", "shell", "echo " + governor + " > /sys/devices/system/cpu/cpufreq/" + policy + "/scaling_governor"])
+        sheet = [["setup core", "runtime core", "frequency", "coverage", "event", "count", "time"]]
 
-        # log
-        print("\n")
-        subprocess.run(["adb", "shell", "cat /sys/devices/system/cpu/cpufreq/policy*/scaling_governor"])
-        print("\n")
-
-        # set frequency parameters
-        subprocess.run(["adb", "shell", "echo " + frequency + " > /sys/devices/system/cpu/cpufreq/policy0/scaling_min_freq"])
-        subprocess.run(["adb", "shell", "echo " + frequency + " > /sys/devices/system/cpu/cpufreq/policy0/scaling_max_freq"])
-
-        # log
-        print("\n")
-        subprocess.run(["adb", "shell", "cat /sys/devices/system/cpu/cpu*/cpufreq/scaling_min_freq"])
-        print("\n")
-        subprocess.run(["adb", "shell", "cat /sys/devices/system/cpu/cpu*/cpufreq/scaling_max_freq"])
-        print("\n")
-        subprocess.run(["adb", "shell", "cat /sys/devices/system/cpu/cpu*/cpufreq/scaling_cur_freq"])
-        print("\n")
-        print(core)
-        print("\n")
-
-        # set benchmark parameters
-        benchmark = {"MiBench": "/data/local/tmp/Mibench/bitcnts 102400000"}
-
-        # set simpleperf parameters
-        simpleperf = "simpleperf stat --use-devfreq-counters --per-core"
-
-        # ouput all counters of raw pmus
-        sheet = [["core", "coverage", "event", "count", "time"]]
-
-        for i in range(0, len(raws), 6):
-            pmus = ",".join(raws[i:i + 6])
-
-            datas = [
-                j for j in [
-                    k.split() for k in filter(
-                        None,
-                        subprocess.run(["adb", "shell", "taskset " +
-                                        core, simpleperf, "-e", pmus, benchmark["MiBench"]], stdout=subprocess.PIPE).stdout.decode('utf-8').splitlines())
-                ] if (j[0] == "0" or j[0] == 'Total')
-            ]
-
-            for j in range(0, len(datas)):
-                if datas[j][0] == "0":
-                    sheet.append([datas[j][0], datas[j][-1].replace("(", "").replace(")", ""), datas[j][2], datas[j][1], datas[-1][3]])
+        for idx, policy in enumerate(policies):
+            core = re.findall(r'\d+', policy)[0]
+            mask = format(1 << int(core), "02x")
 
             # log
-            for j in range(len(sheet)):
-                print('\t'.join(sheet[j]))
+            print("\nset cluster")
+            print(idx)
+            print("\nset core: ")
+            print(core)
+            print("\nset mask: ")
+            print(mask)
             print("\n")
 
-        # ouput csv of all counters
+            for frequency in [frequencies[idx][0], frequencies[idx][int((len(frequencies[idx]) - 1) / 2)], frequencies[idx][-1]]:
+                # log
+                print("\nprepared frequency: ")
+                print(frequency)
+                print("\n")
+
+                # set policy parameters
+                subprocess.run(["adb", "shell", "echo 0 > /sys/devices/system/cpu/cpufreq/" + policy + "/scaling_min_freq"])
+                subprocess.run(["adb", "shell", "echo 99999999 > /sys/devices/system/cpu/cpufreq/" + policy + "/scaling_max_freq"])
+                subprocess.run(["adb", "shell", "echo " + governor + " > /sys/devices/system/cpu/cpufreq/" + policy + "/scaling_governor"])
+
+                # log
+                print("\nset governor: ")
+                subprocess.run(["adb", "shell", "cat /sys/devices/system/cpu/cpufreq/policy*/scaling_governor"])
+                print("\n")
+
+                # set frequency parameters
+                subprocess.run(["adb", "shell", "echo " + frequency + " > /sys/devices/system/cpu/cpufreq/" + policy + "/scaling_min_freq"])
+                subprocess.run(["adb", "shell", "echo " + frequency + " > /sys/devices/system/cpu/cpufreq/" + policy + "/scaling_max_freq"])
+
+                # log
+                print("\nset min frequency: ")
+                subprocess.run(["adb", "shell", "cat /sys/devices/system/cpu/cpu*/cpufreq/scaling_min_freq"])
+                print("\nset max frequency: ")
+                subprocess.run(["adb", "shell", "cat /sys/devices/system/cpu/cpu*/cpufreq/scaling_max_freq"])
+                print("\nset cur frequency: ")
+                subprocess.run(["adb", "shell", "cat /sys/devices/system/cpu/cpu*/cpufreq/scaling_cur_freq"])
+                print("\n")
+
+                # set benchmark parameters
+                benchmark = {"MiBench": "/data/local/tmp/Mibench/bitcnts 102400000"}
+
+                # set simpleperf parameters
+                simpleperf = "simpleperf stat --use-devfreq-counters --per-core"
+
+                # set taskset
+                taskset = "taskset " + mask
+
+                # ouput all counters of raw pmus
+                for i in range(0, len(raws), 6):
+                    pmus = ",".join(raws[i:i + 6])
+
+                    datas = [
+                        j for j in [
+                            k.split() for k in filter(
+                                None,
+                                subprocess.run(["adb", "shell", taskset, simpleperf, "-e", pmus, benchmark["MiBench"]], stdout=subprocess.PIPE).stdout.decode(
+                                    'utf-8').splitlines())
+                        ] if (j[0] == core or j[0] == 'Total')
+                    ]
+
+                    # log
+                    print("\ncommand: ")
+                    print("adb", "shell", taskset, simpleperf, "-e", pmus, benchmark["MiBench"])
+                    print("\n")
+
+                    # out sheet of all counters
+                    for j in range(0, len(datas)):
+                        if datas[j][0] == core:
+                            sheet.append([core, datas[j][0], frequency, datas[j][-1].replace("(", "").replace(")", ""), datas[j][2], datas[j][1], datas[-1][3]])
+
+                    # log
+                    for j in range(len(sheet)):
+                        print('\t'.join(sheet[j]))
+                    print("\n")
+
+        # ouput csv of sheet
         with open("infos.csv", "w") as f:
             w = csv.writer(f, dialect='excel')
 
