854f697090db3b939008cbe5ad99aef93aa97411
misakisuna705 <misakisuna705@gmail.com>
2022-11-13 17:35:15 +0800

    commit

---
+++ src/selector.py
@@ -0,0 +1,105 @@
+#! /usr/bin/env python3
+
+import csv
+import logging
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="[%(asctime)s line:%(lineno)-3d %(levelname)-8s ] %(message)s",
+    datefmt="%Y/%m/%d %H:%M:%S",
+)
+
+
+class Selector(object):
+
+    def __init__(self) -> None:
+        pass
+
+    def select(self):
+        sheets = [
+            "bin/Dhrystone/dry.csv", "bin/LMbench/bw_mem/512m/bcopy.csv", "bin/LMbench/bw_mem/512m/bzero.csv", "bin/LMbench/bw_mem/512m/cp.csv",
+            "bin/LMbench/bw_mem/512m/fcp.csv", "bin/LMbench/bw_mem/512m/frd.csv", "bin/LMbench/bw_mem/512m/rd.csv", "bin/Mibench/bitcnts/84600000.csv",
+            "bin/SpecCpu2006/400.perlbench.csv", "bin/SpecCpu2006/401.bzip2.csv", "bin/SpecCpu2006/403.gcc.csv", "bin/SpecCpu2006/429.mcf.csv",
+            "bin/SpecCpu2006/433.milc.csv", "bin/SpecCpu2006/444.namd.csv", "bin/SpecCpu2006/445.gobmk.csv", "bin/SpecCpu2006/447.dealII.csv",
+            "bin/SpecCpu2006/450.soplex.csv", "bin/SpecCpu2006/453.povray.csv", "bin/SpecCpu2006/456.hmmer.csv", "bin/SpecCpu2006/458.sjeng.csv",
+            "bin/SpecCpu2006/462.libquantum.csv", "bin/SpecCpu2006/464.h264ref.csv", "bin/SpecCpu2006/470.lbm.csv", "bin/SpecCpu2006/471.omnetpp.csv",
+            "bin/SpecCpu2006/473.astar.csv", "bin/SpecCpu2006/482.sphinx3.csv", "bin/SpecCpu2006/483.xalancbmk.csv", "bin/SpecCpu2017/600.perlbench_s.csv",
+            "bin/SpecCpu2017/602.gcc_s.csv", "bin/SpecCpu2017/605.mcf_s.csv", "bin/SpecCpu2017/619.lbm_s.csv", "bin/SpecCpu2017/620.omnetpp_s.csv",
+            "bin/SpecCpu2017/623.xalancbmk_s.csv", "bin/SpecCpu2017/625.x264_s.csv", "bin/SpecCpu2017/638.imagick_s.csv", "bin/SpecCpu2017/641.leela_s.csv",
+            "bin/SpecCpu2017/644.nab_s.csv", "bin/SpecCpu2017/657.xz_s.csv"
+        ]
+
+        print(len(sheets))
+
+        workloads = []
+
+        for sheet in sheets:
+            with open(sheet, newline="") as f:
+                rows = csv.DictReader(f)
+
+                workloads.append([row for row in rows])
+
+        # print(workloads[0])
+
+        cores = sorted(set([row["setup core"] for row in workloads[0]]))
+
+        frequencies = []
+
+        for core in cores:
+            frequencies.append(sorted(set([int(row["frequency"]) for row in workloads[0] if row["setup core"] == core])))
+
+        pmus = sorted(set([row["event"] for row in workloads[0]]))
+
+        # print(cores)
+        # print(frequencies)
+        # print(pmus)
+
+        samples = [[[{"count": [], "time": []} for k in range(len(pmus))] for j in range(len(frequencies[i]))] for i in range(len(cores))]
+
+        for workload in workloads:
+            for i in range(len(cores)):
+                for j in range(len(frequencies[i])):
+                    for k in range(len(pmus)):
+                        row = workload[i * len(workload) // len(cores) + j * len(pmus) + k]
+
+                        samples[i][j][k]["count"].append(int(row["count"].replace(',', '')))
+                        samples[i][j][k]["time"].append(float(row["time"].replace(',', '')))
+
+        # for i, core in enumerate(samples):
+        # for j, frequency in enumerate(core):
+        # for k, pmu in enumerate(frequency):
+        # print("%3d" % (i * len(workloads[0]) // len(cores) + j * len(pmus) + k) + ". ", cores[i], frequencies[i][j], pmus[k], samples[i][j][k])
+
+        def rSquare(listA, listB):
+            aAvg = sum(listA) / len(listA)
+            bAvg = sum(listB) / len(listB)
+
+            covariance = sum([(x - aAvg) * (y - bAvg) for x, y in zip(listA, listB)])
+            stdDeviationProduct = (sum([(x - aAvg)**2 for x in listA]) * sum([(y - bAvg)**2 for y in listB]))**0.5
+
+            return (covariance / stdDeviationProduct)**2 if stdDeviationProduct else 0
+
+        for i in range(len(cores)):
+            for j in range(len(frequencies[i])):
+                for k in range(len(pmus)):
+                    samples[i][j][k]["correlation"] = rSquare(samples[i][j][k]["count"], samples[i][j][k]["time"])
+
+        for i in range(len(cores)):
+            for j in range(len(frequencies[i])):
+                samples[i][j] = sorted(samples[i][j], key=lambda x: x["correlation"], reverse=True)
+
+        for i, core in enumerate(samples):
+            for j, frequency in enumerate(core):
+                for k in range(6):
+                    print("%3d" % (i * len(workloads[0]) // len(cores) + j * len(pmus) + k) + ". ", cores[i], frequencies[i][j], pmus[k], samples[i][j][k]["correlation"])
+                print("")
+
+
+def main():
+    selector = Selector()
+
+    selector.select()
+
+
+if __name__ == "__main__":
+    main()
