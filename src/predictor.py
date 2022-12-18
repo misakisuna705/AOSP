c84cf0f9b832a4f77e1d94154cbb1d2110ccea76
misakisuna705 <misakisuna705@gmail.com>
2022-12-19 03:57:53 +0800

    commit

--- src/predictor.py
+++ src/predictor.py
@@ -1,5 +1,7 @@
 #! /usr/bin/env python3
 
+# import matplotlib.pyplot
+
 import pandas as pd
 import sklearn.linear_model
 import sklearn.metrics
@@ -19,6 +21,11 @@ class Predictor(object):
         # print(dataframe)
         # print("")
 
+        dataframe.drop(dataframe[dataframe['times'] < 1.0].index, axis=0, inplace=True)  # filter
+
+        # print(dataframe)
+        # print("")
+
         self._predictRegression(dataframe)
 
     def _predictRegression(self, dataframe):
@@ -31,16 +38,34 @@ class Predictor(object):
 
         X_train, X_test, y_train, y_test = sklearn.model_selection.train_test_split(X, y, train_size=0.8, random_state=0)
 
-        regressor = sklearn.linear_model.LinearRegression()
+        model = sklearn.linear_model.LinearRegression().fit(X_train, y_train)
 
-        model = regressor.fit(X_train, y_train)
+        # print("coefficient weights: ", model.coef_.tolist())
+        # print("intercept bias: ", model.intercept_)
+        # print("robustness R²: ", model.score(X, y))
+        # print("")
 
-        print("coefficient weights: ", model.coef_.tolist())
-        print("intercept bias: ", model.intercept_)
-        print("robustness rsquare: ", model.score(X, y))
+        y_pred = model.predict(X_test)
+
+        # print(y_test.to_frame().assign(y_pred=y_pred))
+        # print("")
+
+        print("R²: ", sklearn.metrics.r2_score(y_test, y_pred))
         print("")
-        print("MAPE: ", sklearn.metrics.mean_absolute_percentage_error(y_test, regressor.predict(X_test)) * 100, "%")
+        # print("Max error: ", sklearn.metrics.max_error(y_test, y_pred), "(s)")
+        # print("Median absolute error: ", sklearn.metrics.median_absolute_error(y_test, y_pred), "(s)")
+        # print("Mean absolute error: ", sklearn.metrics.mean_absolute_error(y_test, y_pred), "(s)")
+        # print("Mean squared error: ", sklearn.metrics.mean_squared_error(y_test, y_pred), "(s²)")
+        # print("")
+        print("Mean absolute percentage error: ", sklearn.metrics.mean_absolute_percentage_error(y_test, y_pred) * 100, "(%)")
         print("")
+
+        # print("test")
+        # a = [39.032870, 178.547973, 0.285950, 273.209226, 52.829471, 0.572401, 1.530566, 19.949575]
+        # b = [39.684027, 179.659327, -0.291524, 270.484430, 51.311926, 0.134621, 1.525343, 20.274504]
+
+        # print(sklearn.metrics.mean_absolute_percentage_error(a, b) * 100, "%")
+        # print("test")
 
     def _regressByTensorflow(self, dataframe):
         # dataset = tf.data.Dataset.from_tensor_slices((dataframe.iloc[:, :-1], dataframe.iloc[:, -1]))
