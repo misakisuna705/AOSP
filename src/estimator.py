6fa4aafb60ea07807dae4683ac4fc8c78f537c81
misakisuna705 <misakisuna705@gmail.com>
2022-12-26 19:43:51 +0800

    commit

--- src/estimator.py
+++ src/estimator.py
@@ -30,12 +30,9 @@ class Estimator(object):
         # print("")
 
         X, y = self._select(dataframe, 6)
-
         X_train, X_test, y_train, y_test = self._split(X, y, 0.8)
-
-        y_pred = self._predict(X_train, X_test, y_train, y_test)
-
-        self._report(y_test, y_pred)
+        model = self._train(X_train, y_train)
+        self._test(X_test, y_test, model)
 
     def _select(self, dataframe, num):
         # selector = sklearn.feature_selection.RFE(estimator=sklearn.linear_model.LinearRegression(), n_features_to_select=num)
@@ -45,43 +42,45 @@ class Estimator(object):
 
         selector.fit_transform(dataframe.iloc[:, :-1], dataframe.iloc[:, -1])
 
-        # print(list(dataframe.iloc[:, selector.get_support(indices=True)].columns))
-        # print("")
-
         return dataframe.iloc[:, selector.get_support(indices=True)], dataframe.iloc[:, -1]
 
     def _split(self, X, y, ratio):
         return sklearn.model_selection.train_test_split(X, y, train_size=ratio, random_state=42)
 
-    def _predict(self, X_train, X_test, y_train, y_test):
-        return self._predictedBySklearnLinearRegression(X_train, X_test, y_train, y_test)
-        # return self._predictedByStatsmodelsOLS(X_train, X_test, y_train, y_test)
+    def _train(self, X_train, y_train):
+        return self._trainedBySklearnLinearRegression(X_train, y_train)
+        # return self._trainedByStatsmodelsOLS(X_train, y_train)
 
-    def _predictedBySklearnLinearRegression(self, X_train, X_test, y_train, y_test):
+    def _trainedBySklearnLinearRegression(self, X_train, y_train):
         model = sklearn.linear_model.LinearRegression().fit(X_train, y_train)
 
-        print("coefficient weights: ", model.coef_.tolist())
-        # print("intercept bias: ", model.intercept_)
-        # print("robustness R²: ", model.score(X_train, y_train))
+        summary = pd.DataFrame(columns=X_train.columns)  # "coefficient weight
+
+        summary.loc[len(summary.index)] = model.coef_.tolist()
+        summary["intercept bias"] = model.intercept_
+        summary["robustness R²"] = model.score(X_train, y_train)
+
+        print(summary)
         print("")
 
-        return model.predict(X_test)
+        return model
 
-    def _predictedByStatsmodelsOLS(self, X_train, X_test, y_train, y_test):
+    def _trainedByStatsmodelsOLS(self, X_train, y_train):
         model = statsmodels.api.OLS(y_train, X_train).fit()
 
-        # print(model.summary())
-        # print("")
-
-        return model.predict(X_test)
-
-    def _report(self, y_test, y_pred):
-        print(y_test.to_frame().assign(y_pred=y_pred))
+        print(model.summary())
         print("")
+
+        return model
+
+    def _test(self, X_test, y_test, model):
+        y_pred = model.predict(X_test)
+
+        # print(y_test.to_frame().assign(y_pred=y_pred))
+        # print("")
 
         print(
             pd.DataFrame({
-                "Sklearn": ["LinearRegression"],
                 "R²": [sklearn.metrics.r2_score(y_test, y_pred)],
                 "Max error (s)": [sklearn.metrics.max_error(y_test, y_pred)],
                 "Median absolute error (s)": [sklearn.metrics.median_absolute_error(y_test, y_pred)],
